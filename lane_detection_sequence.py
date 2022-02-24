a1143524678c159bf07ccd1d8e16445f16ce8a5d
ORCT <sonic407@naver.com>
2022-02-24 19:47:07 +0900

    hough

--- lane_detection_sequence.py
+++ lane_detection_sequence.py
@@ -10,22 +10,31 @@ def gaussian_blur(img, kernel_size):
 def canny(img, low_threshold, high_threshold):
     return cv2.Canny(img, low_threshold, high_threshold)
 
-def roi(img):
+def roi(img,h,w):
     mask = np.zeros_like(img)
-    h,w = mask.shape
     vertices = np.array([[(w/10,h), (w/10,h*3/4), (w*9/10,h*3/4), (w*9/10,h)]], dtype=np.int32)
     cv2.fillPoly(mask, vertices, 255)
     roi_img = cv2.bitwise_and(img, mask)
     return roi_img
 
+def hough(img,h,w):
+    lines = cv2.HoughLinesP(img, rho=1, theta=np.pi/180, threshold=30, minLineLength=10, maxLineGap=30)
+    line_img = np.zeros((h, w, 3), dtype=np.uint8)
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(line_img, (x1, y1), (x2, y2), color=[255,0,0], thickness=2)
+    return line_img
+
 if __name__ == '__main__':
     origin_img = cv2.imread('./lane_detection_sample.jpg')
+    h,w = origin_img.shape[:2]
     gray_img = grayscale(origin_img)
     blur_img = gaussian_blur(gray_img, 5)
     canny_img = canny(blur_img, 50, 200)
-    roi_img = roi(canny_img)
+    roi_img = roi(canny_img,h,w)
+    hough_img = hough(roi_img,h,w)
 
-    cv2.imwrite('./roi_img.jpg',roi_img)
-    cv2.imshow('roi',roi_img)
+    cv2.imwrite('./hough_img.jpg',hough_img)
+    cv2.imshow('hough',hough_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
